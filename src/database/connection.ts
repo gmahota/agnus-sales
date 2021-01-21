8846d57b607396fb0c6d7a3d06ea537c050efbcf
Guimarães Mahota <guimaraesmahota@gmail.com>
2021-01-21 16:45:58 +0200

    'Heroku'

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,5 +1,9 @@
-import { createConnection } from 'typeorm';
+import {getConnectionManager, ConnectionManager,createConnection } from 'typeorm';
 import ConnectionOptions from '../ormconfig'
 
-console.log(ConnectionOptions)
 createConnection(ConnectionOptions);
+
+const connectionManager = getConnectionManager();
+const connection = connectionManager.create(ConnectionOptions);
+
+connection.connect(); // performs connection
