4fce89c8c04d2cec1fd5a48d0eddc9af5feebf37
Guimarães Mahota <guimaraesmahota@gmail.com>
2021-08-20 17:26:44 +0200

    init utn project

--- ormconfig.js
+++ ormconfig.js
@@ -1,15 +1,17 @@
-module.exports =  [{
-  type: 'mysql',
-  host: process.env.TYPEORM_HOST,
-  port: process.env.TYPEORM_PORT,
-  username: process.env.TYPEORM_USERNAME,
-  password: process.env.TYPEORM_PASSWORD,
-  database: process.env.TYPEORM_DATABASE,
-  entities: [process.env.TYPEORM_ENTITIES],
-  migrations: [process.env.TYPEORM_MIGRATIONS],
-  cli: {
-    migrationsDir: process.env.TYPEORM_MIGRATIONS_DIR,
+module.exports = [
+  {
+    type: "postgres",
+    host: process.env.TYPEORM_HOST,
+    port: process.env.TYPEORM_PORT,
+    username: process.env.TYPEORM_USERNAME,
+    password: process.env.TYPEORM_PASSWORD,
+    database: process.env.TYPEORM_DATABASE,
+    entities: [process.env.TYPEORM_ENTITIES],
+    migrations: [process.env.TYPEORM_MIGRATIONS],
+    cli: {
+      migrationsDir: process.env.TYPEORM_MIGRATIONS_DIR,
+    },
+    logging: process.env.TYPEORM_logging,
+    synchronize: process.env.TYPEORM_synchronize,
   },
-  logging: process.env.TYPEORM_logging,
-  synchronize: process.env.TYPEORM_synchronize
-}]
+];
