d9db96791597f5f7f2f660da1cd8def1c35edf7e
Guimarães Mahota <guimaraesmahota@gmail.com>
2021-08-20 21:04:25 +0200

    config typeorm

--- ormconfig.js
+++ ormconfig.js
@@ -12,6 +12,6 @@ module.exports = [
       migrationsDir: process.env.TYPEORM_MIGRATIONS_DIR,
     },
     logging: process.env.TYPEORM_logging,
-    synchronize: process.env.TYPEORM_synchronize,
+    synchronize: true,
   },
 ];
