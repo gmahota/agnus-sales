aa7f63a74c084b2affd26fd7e57d10798bc6c11d
gmahota <guimaraesmahota@gmail.com>
2024-05-18 11:20:14 +0200

    install shadcn ui

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google"
+
+
+import { cn } from "@/lib/utils"
+ 
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+export const metadata: Metadata = {
+  title: "Agnus Sales App",
+  description: "Simple Customer Services build with NodeJs",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={cn("min-h-screen bg-background font-sans antialiased",fontSans.variable)}>
+        {children}</body>
+    </html>
+  );
+}
